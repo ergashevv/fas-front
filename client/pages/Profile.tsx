2eb4638c3a7e8482caf4d9297f3412068be9102e
ergashevv <ergashevpulot@gmail.com>
2025-10-29 21:02:25 +0500

    dasa

--- client/pages/Profile.tsx
+++ client/pages/Profile.tsx
@@ -69,15 +69,7 @@ export default function Profile() {
                   <p className="font-semibold text-gray-900">{user.phone}</p>
                 </div>
               </div>
-              {user.email && (
-                <div className="flex items-center gap-3 p-4 bg-gray-50 rounded-lg">
-                  <Mail className="w-5 h-5 text-purple-600" />
-                  <div>
-                    <p className="text-sm text-gray-600">Email</p>
-                    <p className="font-semibold text-gray-900">{user.email}</p>
-                  </div>
-                </div>
-              )}
+             
             </div>
           </div>
 
